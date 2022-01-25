d6b925bc806d85a65b842ef199d06e73909a3c92
moe <moerahmeh95@hotmail.com>
2022-01-25 18:14:17 +0200

    Minor, fix typo.

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Home = () => {
           <meta name="image-to-css" content="Convert image to CSS" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap"
             rel="stylesheet"
